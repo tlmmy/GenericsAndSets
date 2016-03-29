16b6f2110bf9db2373e690aa509987981c921e6d
trogers8 <trogers8@L11702042765.wctc.msft>
2016-03-29 14:35:13 -0500

    first commit

---
+++ src/common/Dog.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package common;
+
+/**
+ *
+ * @author instlogin
+ */
+public class Dog {
+    private String name;
+    private int rabiesId;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int rabiesId) {
+        this.name = name;
+        this.rabiesId = rabiesId;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRabiesId() {
+        return rabiesId;
+    }
+
+    public void setRabiesId(int rabiesId) {
+        this.rabiesId = rabiesId;
+    }
+
+    
+
+}
---
+++ src/common/Employee.java
@@ -0,0 +1,43 @@
+package common;
+
+import java.util.*;
+
+public class Employee {
+    private String lastName;
+    private String firstName;
+    private String ssn;
+
+    public Employee(String lastName, String firstName, String ssn) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.ssn = ssn;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    
+}
